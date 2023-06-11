4eaf91cbb382a3cf92d195cf48fa51aab1c5ec19
BladeDancer <69kir969@mail.ru>
2023-06-11 19:05:43 +0300

    First commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const selectDirection = document.getElementById("direction");
+const containerNavBar = document.querySelector(".container_nav_bar");
+const logo = document.querySelector(".logo>a>img");
+const navBarMenu = document.querySelectorAll(".menu_item");
+const navBarTel = document.querySelector(".menu_tel");
+
+selectDirection.addEventListener("change", () => {
+  selectDirection.style.color = selectDirection.selectedIndex && "black";
+});
+
+window.addEventListener("scroll", () => {
+  const isScrolled = window.scrollY > 450;
+  changeClass(containerNavBar, "scrolled_container_nav_bar", isScrolled);
+  changeClass(navBarTel, "scrolled_nav_bar_tel", isScrolled);
+  logo.src = `/img1920/header/YourTour_${isScrolled ? "black" : "white"}.svg`;
+  navBarMenu.forEach((item) => changeClass(item, "scrolled_nav_bar_menu", isScrolled));
+});
+
+const changeClass = (elem, className, isScrolled) => {
+  console.log(elem);
+  isScrolled ? elem.classList.add(className) : elem.classList.remove(className);
+};
