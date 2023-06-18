89448322133c6d6d7185933e9ea4277b7bafc5fc
BladeDancer <69kir969@mail.ru>
2023-06-18 17:31:26 +0300

    correction

--- index.js
+++ index.js
@@ -3,6 +3,16 @@ const containerNavBar = document.querySelector(".container_nav_bar");
 const logo = document.querySelector(".logo>a>img");
 const navBarMenu = document.querySelectorAll(".menu_item");
 const navBarTel = document.querySelector(".menu_tel");
+const selectedVariantTour = document.querySelectorAll(".your_tour_variants a");
+
+for (let i = 0; i < selectedVariantTour.length; i++) {
+  selectedVariantTour[i].addEventListener("click", () => {
+    for (let k = 0; k < selectedVariantTour.length; k++) {
+      selectedVariantTour[k].classList.remove("active");
+    }
+    selectedVariantTour[i].classList.add("active");
+  });
+}
 
 selectDirection.addEventListener("change", () => {
   selectDirection.style.color = selectDirection.selectedIndex && "black";
