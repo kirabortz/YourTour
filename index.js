d4013676ebe25855ab8cf7d3a2db09fdd3011a8e
BladeDancer <69kir969@mail.ru>
2023-07-11 20:19:24 +0300

    commit

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
-const selectDirection = document.getElementById("direction");
-const containerNavBar = document.querySelector(".container_nav_bar");
+const header = document.querySelector(".header");
 const logo = document.querySelector(".logo>a>img");
-const navBarMenu = document.querySelectorAll(".menu_item");
-const navBarTel = document.querySelector(".menu_tel");
 const selectedVariantTour = document.querySelectorAll(".your_tour_variants a");
 
 for (let i = 0; i < selectedVariantTour.length; i++) {
@@ -14,16 +11,10 @@ for (let i = 0; i < selectedVariantTour.length; i++) {
   });
 }
 
-selectDirection.addEventListener("change", () => {
-  selectDirection.style.color = selectDirection.selectedIndex && "black";
-});
-
 window.addEventListener("scroll", () => {
   const isScrolled = window.scrollY > 450;
-  changeClass(containerNavBar, "scrolled_container_nav_bar", isScrolled);
-  changeClass(navBarTel, "scrolled_nav_bar_tel", isScrolled);
+  changeClass(header, "scrolled_container_nav_bar", isScrolled);
   logo.src = `./img1920/header/YourTour_${isScrolled ? "black" : "white"}.svg`;
-  navBarMenu.forEach((item) => changeClass(item, "scrolled_nav_bar_menu", isScrolled));
 });
 
 const changeClass = (elem, className, isScrolled) => {
