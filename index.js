15ded55d8e63931cc4e10503f1ce7db303fc80af
BladeDancer <69kir969@mail.ru>
2023-08-14 17:59:01 +0300

    commit

--- index.js
+++ index.js
@@ -1,20 +1,19 @@
 const header = document.querySelector(".header");
-const logo = document.querySelector(".logo>img");
+const logo = document.querySelectorAll("#logo_img path");
 const selectedVariantTour = document.querySelectorAll(".variant_link");
 
-for (let i = 0; i < selectedVariantTour.length; i++) {
-  selectedVariantTour[i].addEventListener("click", () => {
-    for (let k = 0; k < selectedVariantTour.length; k++) {
-      selectedVariantTour[k].classList.remove("active");
-    }
-    selectedVariantTour[i].classList.add("active");
+selectedVariantTour.forEach((elem) => {
+  elem.addEventListener("click", () => {
+    selectedVariantTour.forEach((item) => {
+      item.classList.remove("active");
+    });
+    elem.classList.add("active");
   });
-}
-
+});
 window.addEventListener("scroll", () => {
   const isScrolled = window.scrollY > 450;
   changeClass(header, "scrolled_container_nav_bar", isScrolled);
-  logo.src = `./img1920/header/YourTour-${isScrolled ? "black" : "white"}.svg`;
+  logo.forEach((item) => (isScrolled ? (item.style.fill = "black") : (item.style.fill = "white")));
 });
 
 const changeClass = (elem, className, isScrolled) => {
