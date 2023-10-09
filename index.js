23fdea5ca3992cb19ed25f368721f086d80627c5
BladeDancer <69kir969@mail.ru>
2023-10-09 18:11:53 +0300

    commit

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const headerBlock = document.querySelector(".h-block");
+const headerBlock = document.querySelector(".header-block");
 const logo = document.querySelectorAll("#logo-img path");
 const selectedVariantTour = document.querySelectorAll(".h-menu__link");
 selectedVariantTour.forEach((elem) => {
